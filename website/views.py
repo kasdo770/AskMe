a53feab55bd7309797a6a67434b78615634e6eaf
DrTr4p <65681890+kasdo770@users.noreply.github.com>
2022-04-04 20:00:06 +0200

    pls work

--- website/views.py
+++ website/views.py
@@ -4,7 +4,6 @@ from flask import Blueprint,redirect,url_for,render_template,request,flash,jsoni
 from flask_login import login_required,logout_user,current_user
 from website import db,urlsafe
 from .model import Post,User,Comment,Like
-import time
 
 views = Blueprint("views", __name__)
 
@@ -52,7 +51,6 @@ def View_Post(id):
                         new_comment = Comment(
                             description=description,
                             author=current_user.id,
-                            datetime = time.ctime,
                             post=post.id
                         )
                         db.session.add(new_comment)
