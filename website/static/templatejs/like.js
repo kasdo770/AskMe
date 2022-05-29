7cba705dc081bb1b4374d3935a21d41b792e1d69
DrTr4p <65681890+kasdo770@users.noreply.github.com>
2022-05-29 13:49:46 +0200

    s

--- website/static/templatejs/like.js
+++ website/static/templatejs/like.js
@@ -1,6 +1,6 @@
 function like(post_id){
     let likeCount = document.getElementById('like-count-' + post_id);
-    let likebutton = document.getElementById('like-button-'  + post_id);
 
-    fetch('/like-post/' + post_id, {method:"POST"}).then((res) => res.json()).then((data) => likeCount.innerHTML = data['likes']);
+    fetch('/like-post/' + post_id, {method:"POST"}).then((res) => res.json()).then((data) => 
+    likeCount.innerHTML = data['likes']);
 }
